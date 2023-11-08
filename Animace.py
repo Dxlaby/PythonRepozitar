db93f179f2cdcc9c930b668b4960967df0c33d48
gabriel <gabriel.hamrle@gmail.com>
2023-11-08 10:47:13 +0100

    Animace idk

--- Animace.py
+++ Animace.py
@@ -1,1 +1,14 @@
+from manim import *;
 
+
+class CreateCircle(Scene):
+    def construct(self):
+        square = Square(color=RED).shift(LEFT * 2)
+        circle = Circle(color=BLUE).shift(RIGHT * 2)
+        self.play(Write(square), Write(circle))
+
+# napsaní čtverce a kruhu na scénu
+#self.play(Write(square), Write(circle))
+
+# schování čtverce a kruhu ze scény
+#self.play(FadeOut(square), FadeOut(circle), run_time=2)
