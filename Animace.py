e522d915e6f1974d80ca2a6bbaf220c0981d90ed
gabriel <gabriel.hamrle@gmail.com>
2023-11-08 11:32:31 +0100

    Hotovo idk

--- Animace.py
+++ Animace.py
@@ -1,14 +1,28 @@
 from manim import *;
 
 
+
 class CreateCircle(Scene):
     def construct(self):
         #square = Square(color=RED).shift(LEFT * 2)
-        circleBlue = Circle(color=BLUE).shift(RIGHT * 2)
-        circleBlue.set_fill(BLUE, opacity=1)
-        circleRed = Circle(color=RED).shift(RIGHT * 1)
+        circleRed = Circle(color=RED).shift(LEFT * 3)
         circleRed.set_fill(RED, opacity=1)
-        self.play(Write(circleRed), Write(circleBlue))
+
+        circleGreen = Circle(color=GREEN)
+        circleGreen.set_fill(GREEN, opacity=1)
+        circleGreen.next_to(circleRed, RIGHT, buff=1)
+
+        circleBlue = Circle(color=BLUE)
+        circleBlue.set_fill(BLUE, opacity=1)
+        circleBlue.next_to(circleGreen, RIGHT, buff=1)
+
+        circles = {circleRed, circleBlue, circleGreen}
+        for circle in circles:
+            self.play(Create(circle))
+        self.wait()
+        for circle1 in circles:
+            for circle2 in circles:
+                self.play(Swap(circle1, circle2), run_time = 1)
 
 # napsaní čtverce a kruhu na scénu
 #self.play(Write(square), Write(circle))
